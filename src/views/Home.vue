6aaba7d7249dff76beb3e5afc0798261832f78d8
qiuzl <doudeer@dingtalk.com>
2019-08-24 12:09:51 +0800

    增加rem

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <div id="read"></div>
+        <span class="icon-bookmark"></span>
+        <span class="icon-close-circle-fill"></span>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
+<style scoped>
+    .icon-bookmark {
+        font-size: 30px;
+        color: red;
+    }
+</style>
